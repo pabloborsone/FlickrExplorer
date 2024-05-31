da2c2eb3354804b46fedb275ba70349da48475ef
Pablo Borsone <pabloborsone@hotmail.com>
2024-05-31 19:34:07 -0300

    Create unit tests for service and view model layer

--- FlickrExplorerTests/FlickrFeedServiceTests.swift
+++ FlickrExplorerTests/FlickrFeedServiceTests.swift
@@ -7,29 +7,73 @@
 
 import XCTest
 
+@testable import FlickrExplorer
+
 final class FlickrFeedServiceTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    let response = """
+    {
+      "title": "Recent Uploads tagged porcupine",
+      "link": "https://www.flickr.com/photos/tags/porcupine/",
+      "description": "",
+      "modified": "2024-05-30T22:05:40Z",
+      "generator": "https://www.flickr.com",
+      "items": [
+        {
+          "title": "Porcupine",
+          "link": "https://www.flickr.com/photos/photosbyblackwolf/53758695904/",
+          "media": {
+            "m": "https://live.staticflickr.com/65535/53758695904_4954b2d59f_m.jpg"
+          },
+          "date_taken": "2024-05-12T20:04:05-08:00",
+          "description": "<p><a href=\\"https://www.flickr.com/people/photosbyblackwolf/\\">Dan King Alaskan Photography</a> posted a photo:</p><p><a href=\\"https://www.flickr.com/photos/photosbyblackwolf/53758695904/\\" title=\\"Porcupine\\"><img src=\\"https://live.staticflickr.com/65535/53758695904_4954b2d59f_m.jpg\\" width=\\"240\\" height=\\"160\\" alt=\\"Porcupine\\" /></a></p><p>This spring, my wife and I have seen an abundance of porcupines. Usually, we may see one or two all year long, in the last week, we have seen 6 different porcupines. (One walked through our yard, of course, I was at work but my wife got a good video of it.)<br />This porcupine was spotted near Wrangell St. Elias National Park. If it wasn't for this porcupine, and one other I photographed later in the day, we would not have seen any wildlife.</p>",
+          "published": "2024-05-30T22:05:40Z",
+          "author": "nobody@flickr.com (\\"Dan King Alaskan Photography\\")",
+          "author_id": "9887740@N02",
+          "tags": "porcupine erethizondorsatum rodent wildlife wilderness protectwildlife preservewilderness wrangellsteliasnationalpark alaska canon80d sigma150600mm dankingalaskanphotographycom"
+        },
+        {
+          "title": "Another Porcupine",
+          "link": "https://www.flickr.com/photos/photosbyblackwolf/53758695905/",
+          "media": {
+            "m": "https://live.staticflickr.com/65535/53758695905_4954b2d59f_m.jpg"
+          },
+          "date_taken": "2024-05-13T10:14:05-08:00",
+          "description": "<p><a href=\\"https://www.flickr.com/people/photosbyblackwolf/\\">Dan King Alaskan Photography</a> posted another photo:</p><p><a href=\\"https://www.flickr.com/photos/photosbyblackwolf/53758695905/\\" title=\\"Another Porcupine\\"><img src=\\"https://live.staticflickr.com/65535/53758695905_4954b2d59f_m.jpg\\" width=\\"240\\" height=\\"160\\" alt=\\"Another Porcupine\\" /></a></p><p>We have seen yet another porcupine near the park. This is truly the year of the porcupine sightings!</p>",
+          "published": "2024-05-31T22:05:40Z",
+          "author": "nobody@flickr.com (\\"Dan King Alaskan Photography\\")",
+          "author_id": "9887740@N02",
+          "tags": "porcupine wildlife alaska"
         }
+      ]
+    }
+    """
+
+
+    func testStatusCode200FetchesFeedItemsSuccessfully() async throws {
+        let sut = makeSUT()
+        let expectedData = response.data(using: .utf8)!
+
+        MockURLProtocol.error = nil
+        MockURLProtocol.requestHandler = { request in
+            let response = HTTPURLResponse(url: URL(string: "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1&tags=porcupine")!,
+                                           statusCode: 200,
+                                           httpVersion: nil,
+                                           headerFields: ["Content-Type": "application/json"])!
+            return (response, expectedData)
+        }
+
+        let items = try await sut.fetch(tag: "porcupine")
+        XCTAssertEqual(items.first?.title, "Porcupine")
     }
 
+    private func makeSUT() -> FlickrFeedService {
+        let sessionConfiguration = URLSessionConfiguration.ephemeral
+        sessionConfiguration.protocolClasses = [MockURLProtocol.self]
+        let mockSession = URLSession(configuration: sessionConfiguration)
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
+        return .init(decoder: decoder, session: mockSession)
+    }
 }
--- FlickrExplorerTests/HomeSearchViewModelTests.swift
+++ FlickrExplorerTests/HomeSearchViewModelTests.swift
@@ -1,17 +1,83 @@
 //
-//  FlickrExplorerTests.swift
+//  HomeSearchViewModelTests.swift
 //  FlickrExplorerTests
 //
 //  Created by Pablo Borsone on 31/05/24.
 //
 
+import Combine
 import XCTest
+
 @testable import FlickrExplorer
 
-final class FlickrExplorerTests: XCTestCase {
-    func testExample() {
+final class HomeSearchViewModelTests: XCTestCase {
+    private var cancellable = Set<AnyCancellable>()
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/YYYY"
 
+        return formatter
+    }()
+
+    override func tearDown() {
+        super.tearDown()
+        cancellable.removeAll()
     }
 
-    private func makeSUT() -> FlickrFeedService
+    @MainActor func testSearchQueryUpdatesHomeItemArrayForExistingTag() {
+        let sut = makeSUT()
+        let expectation = XCTestExpectation(description: "Waiting for items to be set")
+
+        sut.$items
+            .dropFirst()
+            .sink { items in
+                XCTAssertEqual(items.first?.title, "title")
+                expectation.fulfill()
+            }
+            .store(in: &cancellable)
+
+        sut.searchQuery = "tree"
+
+        wait(for: [expectation], timeout: 1)
+    }
+
+    @MainActor func testSearchQueryDoesNotUpdateHomeItemArrayIfTagDoesntExist() {
+        let sut = makeSUT()
+        let expectation = XCTestExpectation(description: "Waiting for items to be set")
+
+        sut.$items
+            .dropFirst()
+            .sink { items in
+                XCTAssertNil(items.first?.title)
+                expectation.fulfill()
+            }
+            .store(in: &cancellable)
+
+        sut.searchQuery = "randomtag"
+
+        wait(for: [expectation], timeout: 1)
+    }
+
+    @MainActor
+    private func makeSUT() -> HomeSearchViewModel {
+        .init(service: MockAPIService(), dateFormatter: dateFormatter, cancellables: cancellable)
+    }
+}
+
+final class MockAPIService: APIService {
+    func fetch(tag: String) async throws -> [FlickrItem] {
+        if tag == "tree" {
+            return [.init(title: "title",
+                          link: URL(string: "https://google.com")!,
+                          media: .init(m: URL(string: "https://google.com")!),
+                          dateTaken: .init(),
+                          description: "",
+                          published: .init(),
+                          author: "Pablo Borsone",
+                          authorID: "m3",
+                          tags: "nature")]
+        } else {
+            return []
+        }
+    }
 }

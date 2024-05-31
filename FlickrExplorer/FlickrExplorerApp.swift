851c41e16124e5e54a61e1c3cdf50d322208d2a3
Pablo Borsone <pabloborsone@hotmail.com>
2024-05-31 10:41:15 -0300

    Initial Commit

---
+++ FlickrExplorer/FlickrExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrExplorerApp.swift
+//  FlickrExplorer
+//
+//  Created by Pablo Borsone on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
